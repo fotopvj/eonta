f30123f0fc9d1e99e9e24bfc2c89fd88192c2412
Forrest Almasi <forrestalmasi@gmail.com>
2016-04-20 00:10:02 -0500

    upload stuff?

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    next();
+});
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,32 @@ var router = express.Router();
 var knox = require('knox');
 
 var s3 = knox.createClient({
-    key: process.env.AS3_ACCESS_KEY,
-    secret: process.env.AS3_SECRET_ACCESS_KEY,
-    bucket: process.env.AS3_BUCKET,
+	key: process.env.AS3_ACCESS_KEY,
+	secret: process.env.AS3_SECRET_ACCESS_KEY,
+	bucket: process.env.AS3_BUCKET,
 });
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('eonta', {});
+	res.render('eonta', {});
 });
 
 router.post('/upload', function(req, res) {
-	console.log(req)
+	var headers = {
+		'x-amz-acl': 'public-read',
+		'Access-Control-Allow-Origin': '*'
+	};
+	req.form.on('part', function(part) {
+		console.log('part',part)
+		headers['Content-Length'] = part.byteCount;
+		s3.putStream(part, part.filename, headers, function(err, s3res) {
+			if (err) {
+				return res.send(500, err);
+			}
+			console.log(s3res);
+			res.render('eonta', {});
+		});
+	});
 });
 
 module.exports = router;
