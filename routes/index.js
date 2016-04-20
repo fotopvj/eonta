1f783b341664adbf6db94e272f6a199ae0e7c0b4
Forrest Almasi <forrestalmasi@gmail.com>
2016-04-19 23:10:15 -0500

    switch from python to node

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var dotenv = require('dotenv');
+dotenv.load();
+
+var express = require('express');
+var router = express.Router();
+var knox = require('knox');
+
+var s3 = knox.createClient({
+    key: process.env.AS3_ACCESS_KEY,
+    secret: process.env.AS3_SECRET_ACCESS_KEY,
+    bucket: process.env.AS3_BUCKET,
+});
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('eonta', {});
+});
+
+router.post('/upload', function(req, res) {
+	console.log(req)
+});
+
+module.exports = router;
